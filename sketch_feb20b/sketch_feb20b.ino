c2b5ee57064af4e18de26bf497d950b509aaec37
Youchun <30670835+youozhan@users.noreply.github.com>
2018-02-20 23:46:07 +0100

    1st update

---
+++ sketch_feb20b/sketch_feb20b.ino
@@ -0,0 +1,38 @@
+#include "Adafruit_Thermal.h" 
+#include "noun_1352964_cc.h"
+#include "SoftwareSerial.h"
+#define TX_PIN 6 // Arduino transmit  YELLOW WIRE  labeled RX on printer
+#define RX_PIN 5 // Arduino receive   GREEN WIRE   labeled TX on printer
+
+SoftwareSerial mySerial(RX_PIN, TX_PIN); // Declare SoftwareSerial obj first
+Adafruit_Thermal printer(&mySerial);     // Pass addr to printer constructor
+
+// -----------------------------------------------------------------------
+
+void setup() {
+
+  // This line is for compatibility with the Adafruit IotP project pack,
+  // which uses pin 7 as a spare grounding point.  You only need this if
+  // wired up the same way (w/3-pin header into pins 5/6/7):
+  pinMode(7, OUTPUT); digitalWrite(7, LOW);
+
+  // NOTE: SOME PRINTERS NEED 9600 BAUD instead of 19200, check test page.
+  mySerial.begin(19200);  // Initialize SoftwareSerial
+  printer.begin();        // Init printer (same regardless of serial type)
+
+  // The following calls are in setup(), but don't *need* to be.  Use them
+  // anywhere!  They're just here so they run one time and are not printed
+  // over and over (which would happen if they were in loop() instead).
+  // Some functions will feed a line when called, this is normal.
+
+  // Print the 75x75 pixel logo in adalogo.h:
+  printer.printBitmap(noun_1352964_cc_width, noun_1352964_cc_height, noun_1352964_cc_data);
+
+  printer.sleep();      // Tell printer to sleep
+  delay(3000L);         // Sleep for 3 seconds
+  printer.wake();       // MUST wake() before printing again, even if reset
+  printer.setDefault(); // Restore printer to defaults
+}
+
+void loop() {
+}
